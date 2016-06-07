f8d1b5370462e5a783f3b3b63b9f0827f8f95f89
Ricardo <ricardozorzal@hotmail.com>
2016-06-07 12:01:03 -0300

    Algoritmo correto adicionado

--- bb.js
+++ bb.js
@@ -10,7 +10,11 @@ function geraCiclo(caminho,no){
 function branchAndBound(graph,sai,chega){
     	var caminhos = [];
     	caminhos.push({caminho: [sai], custo: 0});
-    	
+
+		console.log("Novo Caminho Adicionado: ");
+		console.log({caminho: [sai], custo: 0});
+		console.log("\n\n");
+
     	while(!caminhos.some( (curr) => curr.caminho.indexOf(chega) != -1 ) && caminhos.length > 0){
     		//Pego o primeiro caminho da fila
     		var caminhoInicio = caminhos[0];
@@ -21,15 +25,15 @@ function branchAndBound(graph,sai,chega){
     		//Adicionando todos os filhos que não fazem ciclo
     		for(var i in graph[ultimoElemento]){
     			//Se não faz ciclo e é filho, adiciona o novo caminho a fila
-    			if(graph[ultimoElemento][i] != undefined && graph[ultimoElemento][i] != "" && !geraCiclo(caminhoInicio.caminho,i)){
-    				var novoCaminho = Object.assign({}, caminhoInicio);
+    			if(graph[ultimoElemento][i] != undefined && graph[ultimoElemento][i] != "" && !caminhoInicio.caminho.some( (curr) => curr == i )){
+    				var novoCaminho = JSON.parse(JSON.stringify(caminhoInicio));
     				novoCaminho.caminho.push(i);
     				novoCaminho.custo = parseInt(novoCaminho.custo) + parseInt(graph[ultimoElemento][i]);
     				caminhos.push(novoCaminho);
     				console.log("Novo Caminho Adicionado: ");
     				console.log(novoCaminho);
     				console.log("\n\n");
-    				
+
     			}
     		}
     		//Ordenando segundo o custo acumulado
@@ -39,7 +43,93 @@ function branchAndBound(graph,sai,chega){
     		console.log("\n\n");
     	}
     	return caminhos;
-    
+
  }
- 
- branchAndBound(JSON.parse('{"1":{"1":"","1092":"","2192":"","2634":"","3024":"4","3864":"","4512":"","7223":"3"},"1092":{"1":"","1092":"","2192":"4","2634":"","3024":"","3864":"","4512":"5","7223":"4"},"2192":{"1":"","1092":"4","2192":"","2634":"","3024":"","3864":"","4512":"","7223":""},"2634":{"1":"","1092":"","2192":"","2634":"","3024":"","3864":"3","4512":"4","7223":""},"3024":{"1":"4","1092":"","2192":"","2634":"","3024":"","3864":"","4512":"2","7223":"5"},"3864":{"1":"","1092":"","2192":"","2634":"3","3024":"","3864":"","4512":"","7223":""},"4512":{"1":"","1092":"5","2192":"","2634":"4","3024":"2","3864":"","4512":"","7223":""},"7223":{"1":"3","1092":"4","2192":"","2634":"","3024":"5","3864":"","4512":"","7223":""}}'),1,7223);
+
+grafo = {
+		S:{
+			S:"",
+			A: 3,
+			B:"",
+			C:"",
+			D:4,
+			E:"",
+			F:"",
+			G:"",
+		},
+		A:{
+			S:3,
+			A:"",
+			B:4,
+			C:"",
+			D:5,
+			E:"",
+			F:"",
+			G:"",
+		},
+		B:{
+			S:"",
+			A: 4,
+			B:"",
+			C:4,
+			D:"",
+			E:5,
+			F:"",
+			G:"",
+		},
+		C:{
+			S:"",
+			A:"",
+			B:4,
+			C:"",
+			D:"",
+			E:"",
+			F:"",
+			G:"",
+		},
+		D:{
+			S:4,
+			A: 5,
+			B:"",
+			C:"",
+			D:"",
+			E:2,
+			F:"",
+			G:"",
+		},
+		E:{
+			S:"",
+			A:"",
+			B:5,
+			C:"",
+			D:2,
+			E:"",
+			F:4,
+			G:"",
+		},
+		F:{
+			S:"",
+			A:"",
+			B:"",
+			C:"",
+			D:"",
+			E:"",
+			F:4,
+			G:3,
+		},
+		G:{
+			S:"",
+			A:"",
+			B:"",
+			C:"",
+			D:"",
+			E:"",
+			F:3,
+			G:"",
+		},
+};
+
+console.log(grafo);
+console.log("\n\n");
+
+ branchAndBound(grafo,'S','G');
